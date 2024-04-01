c3642ed709fff742e622986ab27413f810c73a8e
Charletontech <121673088+Charletontech@users.noreply.github.com>
2024-04-02 00:37:23 +0100

    Add files via upload

---
+++ index.dart
@@ -0,0 +1,15 @@
+int addTwo(a, b) {
+  return a + b;
+}
+
+int len(str) {
+  return str.length;
+}
+
+String getFirstElement(li) {
+  return li[0];
+}
+
+void main() {
+  print(getFirstElement(['james']));
+}
